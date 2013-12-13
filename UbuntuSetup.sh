581f7ab0548f3f5a24baaf000ff263f3334ed18d
bfrasure <bill.frasure@gmail.com>
2013-12-13 13:47:06 -0700

    -Finally got the list of steps needed to compile ShapesLibrary stored in a script file.

---
+++ UbuntuSetup.sh
@@ -0,0 +1,20 @@
+sudo apt-get install vim
+sudo apt-get install screen
+sudo apt-get install g++
+sudo apt-get install libboost-all-dev
+#Instructions for setting up opengl here: www.wikihow.com/Install-Mesa-(openGL)-on-Linux-Mint
+sudo apt-get install freeglut3-dev
+sudo apt-get install cmake
+sudo apt-get install libglew-dev
+sudo apt-get install mesa-common-dev
+sudo apt-get install build-essential
+sudo apt-get install libglew1.5-dev libglm-dev
+sudo apt-get install libxmu-dev
+#To solve "could not find working GL lib" error
+sudo apt-get install libxi-dev libxine-dev
+
+
+sudo apt-get install imagemagick
+sudo apt-get install libmagick++-dev
+
+wget http://plib.sourceforge.net/dist/plib-1.8.5.tar.gz
