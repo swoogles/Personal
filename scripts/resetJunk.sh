3471ac10f2b0947695ce91b18fff9b35619d87ce
swoogles <bill.frasure@gmail.com>
2013-12-19 12:06:36 -0700

    -Fixed resetJunk script

--- scripts/resetJunk.sh
+++ scripts/resetJunk.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-#Alternate
-echo "Old"
-gs | awk ' match($2, /modified:/) { print $3 } ' | grep -v ApplicationResources\.properties grep -e properties -e ant-deploy\.xml -e build-impl\.xml
-echo "new"
-git status | grep modified | grep -v ApplicationResources\.properties | grep -e properties -e ant-deploy\.xml -e build-impl\.xml | awk ' { print $3 } '
+# new
+git checkout `git status | awk ' match($2, /modified:/) { print $3 } ' | grep -v "ApplicationResources\.properties" | grep -e properties -e ant-deploy\.xml -e build-impl\.xml`
+# old
 # git checkout `git status | grep modified | grep -v ApplicationResources\.properties | grep -e properties -e ant-deploy\.xml -e build-impl\.xml | awk ' { print $4 } '`
-# git checkout srtest/build.xml
-
