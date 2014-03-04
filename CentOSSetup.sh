d0f6d550232c25bc4a62b6aa9f23b75de0d062bb
swoogles <bill.frasure@gmail.com>
2014-03-03 22:14:07 -0700

    Improve CentOS script. It's still really bad.

--- CentOSSetup.sh
+++ CentOSSetup.sh
@@ -1,30 +1,32 @@
-sudo yum install evince
+INSTALL="sudo yum install"
+
+$INSTALL evince
 sudo yum groupinstall 'Development tools'
-sudo yum install -y ncurses-devel readline-devel openssl-devel zlib-devel rpm-build autoconf libtool gcc-c++
+$INSTALL -y ncurses-devel readline-devel openssl-devel zlib-devel rpm-build autoconf libtool gcc-c++
 sudo yum update
 sudo yum upgrade
-sudo yum install java-1.7.0-openjdk-devel
-sudo yum install cscope
-sudo yum install dia
-sudo yum install google-chrome
-sudo yum install chrome
-sudo yum install R-dev
-sudo yum install R-devel
+$INSTALL java-1.7.0-openjdk-devel
+$INSTALL cscope
+$INSTALL dia
+$INSTALL google-chrome
+$INSTALL chrome
+$INSTALL R-dev
+$INSTALL R-devel
 man yum-update
 sudo yum info R-devel
 sudo yum repolist
 sudo yum history
 man yum
-sudo yum install grc
-sudo yum install xcb
-sudo yum install xcb-util-devel
-sudo yum install pango-devel
-sudo yum install yajl-devel
-sudo yum install startup-notification-devel
-sudo yum install pcre-devel
-sudo yum install ev-devel
-sudo yum install ev
-sudo yum install libev-devel
+$INSTALL grc
+$INSTALL xcb
+$INSTALL xcb-util-devel
+$INSTALL pango-devel
+$INSTALL yajl-devel
+$INSTALL startup-notification-devel
+$INSTALL pcre-devel
+$INSTALL ev-devel
+$INSTALL ev
+$INSTALL libev-devel
 yi gthumb
 yi eigen
 yi lynx
