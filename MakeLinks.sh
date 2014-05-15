b86564ad4fdafe8f769bf0a305087c1e542fb33c
Bill Frasure <bill@solutionreach.com>
2014-05-15 15:10:56 -0600

    Improve MakeLinks script
    I need to be using this all the time now and completely stop working
    with copies of files in my home directory and this repository.

--- MakeLinks.sh
+++ MakeLinks.sh
@@ -1,5 +1,8 @@
-ln .speedswapper ~/.speedswapper
-ln .bashrc ~/.bashrc
-ln .bash_profile ~/.bash_profile
-ln .vimrc  ~/.vimrc 
-ln .screenrc  ~/.screenrc 
+rm ~/.speedswapper; ln .speedswapper ~/.speedswapper
+rm ~/.bashrc; ln .bashrc ~/.bashrc
+rm ~/.bash_profile; ln .bash_profile ~/.bash_profile
+rm ~/.vimrc; ln .vimrc  ~/.vimrc 
+rm ~/.screenrc; ln .screenrc  ~/.screenrc 
+rm ~/.bash_colors; ln .bash_colors ~/.bash_colors
+rm ~/.git-completion.bash; ln .git-completion.bash ~/.git-completion.bash
+rm ~/.pentadactylrc; ln .pentadactylrc ~/.pentadactylrc
