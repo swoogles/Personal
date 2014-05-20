81b5f658c8a86351923bb8f68e7a4358c66929e3
Bill Frasure <bill@solutionreach.com>
2014-05-20 14:36:15 -0600

    Add extra file to delete

--- scripts/resetJunk.sh
+++ scripts/resetJunk.sh
@@ -4,3 +4,4 @@
 git checkout `git status | awk ' match($2, /modified:/) { print $3 } ' | grep -v "ApplicationResources\.properties" | grep -e properties -e ant-deploy\.xml -e build-impl\.xml`
 # old
 # git checkout `git status | grep modified | grep -v ApplicationResources\.properties | grep -e properties -e ant-deploy\.xml -e build-impl\.xml | awk ' { print $4 } '`
+rm appSubscriber/nbproject/build-impl.xml~
