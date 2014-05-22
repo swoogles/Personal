1244091ec83386c50a152a6647ca92b6792de20c
Bill Frasure <bill@solutionreach.com>
2014-05-22 08:19:23 -0600

    Add more files to resetJunk.sh
    Not sure why this list is growing now...

--- scripts/resetJunk.sh
+++ scripts/resetJunk.sh
@@ -4,4 +4,7 @@
 git checkout `git status | awk ' match($2, /modified:/) { print $3 } ' | grep -v "ApplicationResources\.properties" | grep -e properties -e ant-deploy\.xml -e build-impl\.xml`
 # old
 # git checkout `git status | grep modified | grep -v ApplicationResources\.properties | grep -e properties -e ant-deploy\.xml -e build-impl\.xml | awk ' { print $4 } '`
+git reset HEAD appSubscriber/nbproject/build-impl.xml~
 rm appSubscriber/nbproject/build-impl.xml~
+git reset HEAD appPatron/nbproject/build-impl.xml~
+rm appPatron/nbproject/build-impl.xml~
