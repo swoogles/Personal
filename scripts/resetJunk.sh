39ebfcb7f254a34a835e50e9361ae9f390856c76
Bill Frasure <bill.frasure@gmail.com>
2014-06-16 07:42:45 -0600

    Fix resetJunk script

--- scripts/resetJunk.sh
+++ scripts/resetJunk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # new
-git checkout `git status | awk ' match($2, /modified:/) { print $3 } ' | grep -v "ApplicationResources\.properties" | grep -e properties -e ant-deploy\.xml -e build-impl\.xml`
+git checkout `git status | awk ' match($1, /modified:/) { print $2 } ' | grep -v "ApplicationResources\.properties" | grep -e properties -e ant-deploy\.xml -e build-impl\.xml`
 # old
 # git checkout `git status | grep modified | grep -v ApplicationResources\.properties | grep -e properties -e ant-deploy\.xml -e build-impl\.xml | awk ' { print $4 } '`
 git reset HEAD appSubscriber/nbproject/build-impl.xml~
