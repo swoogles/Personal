0a49ddb3c3bc5a3b4c457aa8697921660309860e
Bill Frasure <bill.frasure@gmail.com>
2014-07-30 01:26:58 -0400

    Add git config email/name lines
    I still need to complete these in a way that doesn't forever put my
    email address in this repo.

--- MintBootStrap.sh
+++ MintBootStrap.sh
@@ -2,3 +2,6 @@ sudo apt-get install git
 mkdir ~/Repositories
 cd ~/Repositories
 git clone https://github.com/swoogles/Personal.git
+# Do this in a private way
+# git config --global user.email "*myEmail*"
+# git config --global user.name "*myName*"
