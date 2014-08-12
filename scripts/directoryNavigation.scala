4e000661a7f370c8726e3af0e7cf6b6628da1ada
Bill Frasure <bill.frasure@gmail.com>
2014-08-12 17:22:04 -0600

    More playing with Scala directory navigation

--- scripts/directoryNavigation.scala
+++ scripts/directoryNavigation.scala
@@ -1,21 +1,34 @@
 import java.io.File
+import java.util.ArrayList
 
-val fileNames = new File(".").listFiles.map(_.getName).toList
-//println(fileNames)
+def getSubDirectories(file:File):Array[File] = {
+  if ( file.isDirectory )
+    file.listFiles.filter(_.isDirectory)
+  else
+    new Array[File](0)
 
-val files = new File(".").listFiles
-//println(files)
+}
 
-      val narrowFileNames = files.filter(_.getName.contains("scala")).map(_.getName).toList
+val subdirectories = getSubDirectories( new File(".") )
 
-//println(narrowFileNames)
 
-      val directoryNames = files.filter(_.isDirectory).map(_.getName).toList
 
-//println(directoryNames)
-
-      val subDirectoryNames = files.filter(_.isDirectory).flatMap(_.listFiles).map(_.getName).toList
-
-println(subDirectoryNames)
-
-println("bunk")
+//val fileNames = new File(".").listFiles.map(_.getName).toList
+////println(fileNames)
+//
+//val files = new File(".").listFiles
+////println(files)
+//
+//      val narrowFileNames = files.filter(_.getName.contains("scala")).map(_.getName).toList
+//
+////println(narrowFileNames)
+//
+//      val directoryNames = files.filter(_.isDirectory).map(_.getName).toList
+//
+////println(directoryNames)
+//
+//      val subDirectoryNames = files.filter(_.isDirectory).flatMap(_.listFiles).map(_.getName).toList.sorted
+//
+//println(subDirectoryNames)
+//
+//println("bunk")
