6d599dd527bcd7676b7418268926198c7a7d7359
bfrasure <bill.frasure@gmail.com>
2014-09-02 14:35:14 -0600

    Add vim dir and .gitconfig file to MakeLinks

--- MakeLinks.sh
+++ MakeLinks.sh
@@ -5,8 +5,10 @@ rm -f ~/.bash_profile; ln .bash_profile ~/.bash_profile
 rm -f ~/.vimrc; ln .vimrc  ~/.vimrc 
 rm -f ~/.screenrc; ln .screenrc  ~/.screenrc 
 rm -f ~/.bash_colors; ln .bash_colors ~/.bash_colors
+rm -f ~/.gitconfig; ln .gitconfig ~/.gitconfig
 rm -f ~/.git-completion.bash; ln .git-completion.bash ~/.git-completion.bash
 rm -f ~/.gitignore_global; ln .gitignore_global ~/.gitignore_global
 rm -f ~/.pentadactylrc; ln .pentadactylrc ~/.pentadactylrc
 rm -f ~/.tmux.conf; ln .tmux.conf ~/.tmux.conf
 rm -Rf ~/scripts; mkdir ~/scripts; cp -R scripts ~/
+rm -Rf ~/.vim; mkdir ~/.vim; cp -R .vim ~/
