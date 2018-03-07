f68b4c4e5d3dad990ca3d621cf7ddbe7880d2cf8
Bill Frasure <bill.frasure@collectivemedicaltech.com>
2018-03-06 21:41:35 -0700

    More Gradle projects & functionality  #time 1h

--- .ammonite/ultraRepl/src/com/billding/clients/Gradle.scala
+++ .ammonite/ultraRepl/src/com/billding/clients/Gradle.scala
@@ -6,8 +6,8 @@ object Gradle extends Client {
   val client = new ClientBuilder("./gradlew")
   def execute(args: String*): Unit = client.execute(args: _*)
 
-  private def findBugsStages = List("findbugsMain", "findbugsTest")
-  private def testStages = List("integrationTest", "test")
+  def findBugsStages = List("findbugsMain", "findbugsTest")
+  def testStages = List("integrationTest", "test")
 
   // Can we get rid of implicit wd: Path on each of these? Should we?
   def build()(implicit wd: Path) =
@@ -27,4 +27,34 @@ object Gradle extends Client {
 
   def monolith()(implicit wd: Path) =
     c("ear")
+
+  object Projects {
+    val EDIECOMMONS = GradleProject("EDIECOMMONS")
+    val EDIEEAR = GradleProject("EDIEEAR")
+    val EDIEEJB = GradleProject("EDIEEJB")
+    val EDIEHAR = GradleProject("EDIEHAR")
+    val EDIEREST = GradleProject("EDIEREST")
+    val EDIEWEB = GradleProject("EDIEWEB")
+    val HL7 = GradleProject("HL7")
+    val Mappers = GradleProject("Mappers")
+    val MLLPEJB = GradleProject("MLLPEJB")
+    val Records = GradleProject("Records")
+  }
+}
+
+case class GradleProject(name: String) {
+  def test()(implicit wd: Path) =
+    Gradle.c(s":$name:test")
+
+  def integrationTest()(implicit wd: Path) =
+    Gradle.c(s":$name:integrationTest")
+
+  def complexTask(stages: List[String]) =
+    Gradle.c(
+      stages.map(stage => s":$name:$stage")
+    )
+
+  def findbugs()(implicit wd: Path) =
+    complexTask(Gradle.findBugsStages)
+
 }
