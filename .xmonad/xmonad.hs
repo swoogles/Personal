8e8a25c5f6028cb444e3d2d45cac5cf7cd15246b
bfrasure <bill.frasure@gmail.com>
2018-09-30 13:36:39 -0600

    Flesh out workspaces. rm junk.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,16 +13,61 @@ import Control.Concurrent
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
 
-communicationProject = Project { projectName      = "Communication"
-            , projectDirectory = "~/Downloads"
-            , projectStartHook = Just $ do spawn "wavebox"
-            }
-ideProject = Project { projectName      = "ide"
-            , projectDirectory = "~/Downloads"
-            , projectStartHook = Just $ do launchIde
-            }
+launchIde :: X()
+launchIde = spawn "/home/bill.frasure/.local/share/JetBrains/Toolbox/apps/IDEA-U/ch-0/182.4323.46/bin/idea.sh"
+
+terminalProject = 
+  Project { 
+    projectName      = "terminal"
+    , projectDirectory = "~/CmtRepositories/dev"
+    , projectStartHook = Just $ do spawn "terminology"
+  }
 
 
+communicationProject = 
+  Project { 
+    projectName      = "communication"
+    , projectDirectory = "~/Downloads"
+    , projectStartHook = Just $ do spawn "wavebox"
+  }
+
+-- Figure out where I can stick this to deal with Intellij startup lag.
+-- Concurrent.threadDelay 10
+ideProject = 
+  Project { 
+      projectName      = "ide"
+      , projectDirectory = "~/Downloads"
+      , projectStartHook = Just $ do launchIde
+  }
+
+databaseProject = 
+  Project { 
+      projectName      = "database"
+      , projectDirectory = "~/Downloads"
+      , projectStartHook = Just $ do spawn "datagrip"
+  }
+
+
+browserProject = 
+  Project { 
+    projectName      = "browser"
+    , projectDirectory = "~/Downloads"
+    , projectStartHook = Just $ do spawn "firefox"
+  }
+
+musicProject = 
+  Project { 
+    projectName      = "music"
+    , projectDirectory = "~/Downloads"
+    , projectStartHook = Just $ do spawn "spotify"
+  }
+
+postmanProject = 
+  Project { 
+    projectName      = "api"
+    , projectDirectory = "~/Downloads"
+    , projectStartHook = Just $ do spawn "~/Postman/Postman"
+  }
 
 projects :: [Project]
 projects =
@@ -30,15 +75,13 @@ projects =
             , projectDirectory = "~/"
             , projectStartHook = Nothing
             }
-
-  , Project { projectName      = "browser"
-            , projectDirectory = "~/Downloads"
-            , projectStartHook = Just $ do spawn "conkeror"
-                                           spawn "chromium"
-            }
-
+  , terminalProject
+  , browserProject
   , ideProject
-  ,  communicationProject 
+  , communicationProject 
+  , databaseProject
+  , musicProject
+  , postmanProject
   ]
 
 myKeys x  = M.union (M.fromList (newKeys x)) (keys defaultConfig x)
@@ -48,18 +91,6 @@ spawnToWorkspace program workspace = do
                                       spawn program     
                                       windows $ W.greedyView workspace
 
-launchCommunication :: X()
-launchCommunication = spawnToWorkspace "wavebox" "4:communication"
-
--- Figure out where I can stick this to deal with Intellij startup lag.
--- Concurrent.threadDelay 10
-
-launchIde :: X()
-launchIde = spawnToWorkspace "/home/bill.frasure/.local/share/JetBrains/Toolbox/apps/IDEA-U/ch-0/182.4323.46/bin/idea.sh" "2:ide"
-
-launchDatagrip :: X()
-launchDatagrip = spawnToWorkspace "datagrip" "6:database"
-
 launchPostman :: X()
 launchPostman = spawnToWorkspace "~/Postman/Postman" "7:postman"
 
@@ -67,65 +98,41 @@ openSprintBoard :: MonadIO m => m()
 openSprintBoard = spawn "firefox https://jira.collectivemedicaltech.com/secure/RapidBoard.jspa?rapidView=69"
 
 --{{{ Keybindings 
---    Add new and/or redefine key bindings
--- TODO Figure out type here...
 -- newKeys :: [((KeyMask, KeySym), X ())]
 newKeys conf@(XConfig {XMonad.modMask = modm}) = [
   -- ((modm, xK_p), spawn "dmenu_run -nb '#3F3F3F' -nf '#DCDCCC' -sb '#7F9F7F' -sf '#DCDCCC'"),  --Uses a colourscheme with dmenu
-  ((modm, xK_b), spawn "firefox")
+  ((modm, xK_b), switchProject browserProject)
   , ((modm, xK_s), openSprintBoard)
   , ((modm, xK_i), switchProject ideProject)
-  , ((modm, xK_c), launchCommunication)
-  , ((modm, xK_d), launchDatagrip)
-  , ((modm, xK_a), launchPostman)
+  , ((modm, xK_d), switchProject databaseProject)
+  , ((modm, xK_m), switchProject musicProject)
+  , ((modm, xK_a), switchProject postmanProject)
+  , ((modm, xK_t), switchProject terminalProject)
   -- , ((modm, xK_space), switchProjectPrompt)
   -- , ((modm, xK_slash), shiftToProjectPrompt)
   -- , ((modm , xK_l), spawn "gnome-power-statistics")
-  , ((mod4Mask, xK_h), windows $ W.greedyView "1:terminal")
-  , ((mod4Mask, xK_j), windows $ W.greedyView "2:ide")
-  , ((mod4Mask, xK_k), windows $ W.greedyView "3:browser")
-  , ((mod4Mask, xK_l), windows $ W.greedyView "4:communication")
-  , ((modm, xK_l), switchProject communicationProject  )
-  -- , ((modm, xK_l), shiftToProjectPrompt )
-  , ((mod4Mask, xK_semicolon), windows $ W.greedyView "5:music")
-  , ((mod4Mask, xK_d), windows $ W.greedyView "6:database")
-	
-  -- , ((xK_Super_L , xK_h), W.shift, 1)
+  , ((modm, xK_c), switchProject communicationProject  )
   -- ((modm, xK_f), spawn "urxvt -e mc"),
-  -- ((modm, xK_g), spawn "chromium --app='https://www.nirvanahq.com/app'"),
   -- ((0, xK_Print), spawn "scrot"),
-  -- ((modm, xK_v), spawn "VirtualBox"),
-  -- ((modm, xK_z), goToSelected myGSConfig),
   -- ((0, xF86XK_AudioMute), spawn "amixer -q set PCM toggle"),
   -- ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q set PCM 2+"),
   -- ((0, xF86XK_AudioLowerVolume), spawn "amixer -q set PCM 2-"),
   -- ((0, xF86XK_AudioPlay), spawn "exaile -t"),
   -- ((0, xF86XK_AudioStop), spawn "exaile -s"),
-  -- ((modm, xK_i), sendMessage MirrorExpand)
    ]
---}}}
 
-myWorkspaces = ["1:terminal", "2:ide", "3:browser", "4:communication", "5:music", "6:database", "7:postman"]
-
+-- TODO Consider deleting this, since I'm using DynamicWorkspaces now
 onScr :: ScreenId -> (WorkspaceId -> WindowSet -> WindowSet) -> WorkspaceId -> X ()
 onScr n f i = screenWorkspace n >>= \sn -> windows (f i . maybe id W.view sn)
 
 configureForSwing :: X()
 configureForSwing = setWMName "LG3D"
 
--- something :: IO()
 -- something =  xmonad $ dynamicProjects projects def
 
 main = xmonad $ ( dynamicProjects projects desktopConfig {
   keys          = myKeys
-  -- , workspaces = myWorkspaces
   , startupHook = onScr 1 W.greedyView "web" <+> configureForSwing
   })
 
--- main = xmonad  =<< xmobar  desktopConfig {
-  -- keys          = myKeys
-  -- , workspaces = myWorkspaces
-  -- , startupHook = onScr 1 W.greedyView "web" <+> configureForSwing
-  -- }
-
-
+-- main = xmonad  =<< xmobar  desktopConfig { ... }
